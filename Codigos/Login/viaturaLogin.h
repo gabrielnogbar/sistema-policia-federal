1d5508f2f649fb86a50f11806be194a602fd27e6
Kauan Cardoso <138890290+KauanPr@users.noreply.github.com>
2023-11-05 17:26:16 -0400

    viaturaLogin.h

--- Codigos/Login/viaturaLogin.h
+++ Codigos/Login/viaturaLogin.h
@@ -1,35 +1,35 @@
 #ifndef ViATURA_LOGIN
 #define MAX 100
 #define ViATURA_LOGIN
-struct Policial{
-    char nome[MAX+1];
-    int CPF;
-    char nomeGuerra[MAX+1];
-    char cidade[MAX+1];
-    int idade;
-    char cargo[MAX+1];
-    char senha[MAX+1];
-    Policial *prox;
-};
-struct Pessoa
-{  char nome[50],cidade[20];
-   int CPF, idade;
-   int numeropassagens;
-   char **passagens[3];
-   int numeroinadimplencias;
-   char **inadimpencias[3];
-   Pessoa *prox;
-};
+    struct Policial{
+        char nome[MAX+1];
+        int CPF;
+        char nomeGuerra[MAX+1];
+        char cidade[MAX+1];
+        int idade;
+        char cargo[MAX+1];
+        char senha[MAX+1];
+        Policial *prox;
+    };
+    struct Pessoa
+    {  char nome[50],cidade[20];
+       int CPF, idade;
+       int numeropassagens;
+       char **passagens[3];
+       int numeroinadimplencias;
+       char **inadimpencias[3];
+       Pessoa *prox;
+    };
+    
+    struct Viatura{
+        int Codigo;
+        char **policiais[4];
+        char Tipo[15];
+        Viatura *prox;
+        int Disponivel; // 0 ocupado 1 desocupado
+        int Logado;// 0 deslogado 1 logado
+    };
+    
+    void LoginViaturas(int op2, Policial *ptr, Viatura *ptrV, chamadoPolicial *ptrR, chamadoPolicial *ptrP);
 
-struct Viatura{
-    int Codigo;
-    char **policiais[4];
-    char Tipo[15];
-    Viatura *prox;
-    int Disponivel; // 0 ocupado 1 desocupado
-    int Logado;// 0 deslogado 1 logado
-};
-
-void LoginViaturas(int op2, Policial *ptr, Viatura *ptrV, chamadoPolicial *ptrR, chamadoPolicial *ptrP);
-
-#endif//
+#endif //
